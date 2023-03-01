8458d953688871912ccc10ce58aa8c14eb44a693
Jake Schilling <jake@schillingcentral.net>
2023-03-01 11:03:03 -0700

    Implement Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    if (args.length < 1) {
-      System.out.println("Incorrect arguments");
+    System.out.println("Welcome to the calculator");
+    System.out.println("Enter a command: ");
+    Scanner scanner = new Scanner(System.in);
+    String input = "";
+    while (!input.equals("exit")) {
+      input = scanner.nextLine();
+      System.out.println(doStuff(input.split(" ")));
     }
-    else {
-      Calculator calculator = new Calculator();
-      switch (args[0])  {
-        case "add":
-          System.out.println(calculator.add(Integer.parseInt(args[1]),Integer.parseInt(args[2])));
-          break;
-        case "subtract":
-          System.out.println(calculator.subtract(Integer.parseInt(args[1]),Integer.parseInt(args[2])));
+  }
+  private static String doStuff(String[] args) {
+    Calculator calculator = new Calculator();
+    switch (args[0])  {
+      case "add":
+        return String.valueOf(calculator.add(Integer.parseInt(args[1]),Integer.parseInt(args[2])));
+      case "subtract":
+        return String.valueOf(calculator.subtract(Integer.parseInt(args[1]),Integer.parseInt(args[2])));
 
-          break;
-        case "multiply":
-          System.out.println(calculator.multiply(Integer.parseInt(args[1]),Integer.parseInt(args[2])));
+      case "multiply":
+        return String.valueOf(calculator.multiply(Integer.parseInt(args[1]),Integer.parseInt(args[2])));
 
-          break;
-        case "divide":
-          System.out.println(calculator.divide(Integer.parseInt(args[1]),Integer.parseInt(args[2])));
+      case "divide":
+        return String.valueOf(calculator.divide(Integer.parseInt(args[1]),Integer.parseInt(args[2])));
 
-          break;
-        case "binary":
-          System.out.println(calculator.intToBinaryNumber(Integer.parseInt(args[1])));
-          break;
-        case "fibonacci":
-          System.out.println(calculator.fibonacciNumberFinder(Integer.parseInt(args[1])));
-          break;
-      }
+      case "binary":
+        return calculator.intToBinaryNumber(Integer.parseInt(args[1]));
+      case "fibonacci":
+        return String.valueOf(calculator.fibonacciNumberFinder(Integer.parseInt(args[1])));
     }
+    return "";
   }
 }
