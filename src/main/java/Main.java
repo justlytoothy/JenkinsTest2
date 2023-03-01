4c49b57976ccf5453375370ddb8bcfd0cc815af5
Jake Schilling <jake@schillingcentral.net>
2023-03-01 10:50:40 -0700

    Implement Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-  public static void Main(String[] args) {
+  public static void main(String[] args) {
     if (args.length < 1) {
       System.out.println("Incorrect arguments");
     }
